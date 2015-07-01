d00e20bc6bb2090d5cdce2e5b13a88028449ba77
Mick <mick@javascript-spreadsheet-programming.com>
2015-07-01 13:34:36 +0100

    Update chapter02_arrays.sql

--- chapter02_arrays.sql
+++ chapter02_arrays.sql
@@ -3,5 +3,5 @@ CREATE TABLE tab_delimited_file(
   data_row TEXT);
 
 -- Run a COPY command from psql to load the tab-delimited file into the table.
- \COPY tab_delimited_file FROM <path to source file WITH DELIMITER '!'
+ \COPY tab_delimited_file FROM <path to source file> WITH DELIMITER '!'
  
