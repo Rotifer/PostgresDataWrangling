6f1177078dbecedbfd8912f1780c87daa38a735e
Mick <mick@javascript-spreadsheet-programming.com>
2015-07-01 17:02:30 +0100

    Update chapter02_arrays.sql

--- chapter02_arrays.sql
+++ chapter02_arrays.sql
@@ -5,3 +5,12 @@ CREATE TABLE tab_delimited_file(
 -- Run a COPY command from psql to load the tab-delimited file into the table.
  \COPY tab_delimited_file FROM <path to source file> WITH DELIMITER '!'
  
+/*
+Turn each data row into an array and 
+get the number of elements
+*/
+SELECT
+  STRING_TO_ARRAY(data_row, E'\t'),
+  ARRAY_LENGTH(STRING_TO_ARRAY(data_row, E'\t'), 1)
+FROM
+  tab_delimited_file;
